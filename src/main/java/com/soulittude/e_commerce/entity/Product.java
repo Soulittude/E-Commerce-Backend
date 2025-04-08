47ff6b6091ce2093ff7927e82ab89920847c9024
soulittude <79145776+Soulittude@users.noreply.github.com>
2025-04-08 23:02:08 +0300

    Product Entity Is Created

--- src/main/java/com/soulittude/e_commerce/entity/Product.java
+++ src/main/java/com/soulittude/e_commerce/entity/Product.java
@@ -1,5 +1,21 @@
 package com.soulittude.e_commerce.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+
 public class Product {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+    private Double price;
 }
