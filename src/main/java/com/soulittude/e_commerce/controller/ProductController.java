e9d8350f422e5b5e9685fba86cd7060c8e753a79
soulittude <79145776+Soulittude@users.noreply.github.com>
2025-04-08 23:05:17 +0300

    Product Controller Created

--- src/main/java/com/soulittude/e_commerce/controller/ProductController.java
+++ src/main/java/com/soulittude/e_commerce/controller/ProductController.java
@@ -1,5 +1,24 @@
 package com.soulittude.e_commerce.controller;
 
+import com.soulittude.e_commerce.entity.Product;
+import com.soulittude.e_commerce.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+@CrossOrigin(origins = "http://localhost:4200") // Allow Angular
+
 public class ProductController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
     
 }
