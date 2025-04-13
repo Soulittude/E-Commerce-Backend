98ca243b99018e755fe3e0012200bac658e7b99c
soulittude <79145776+Soulittude@users.noreply.github.com>
2025-04-13 22:16:30 +0300

    Remove Lombok annotations and inner DTO classes from AuthController

--- src/main/java/com/soulittude/e_commerce/controller/AuthController.java
+++ src/main/java/com/soulittude/e_commerce/controller/AuthController.java
@@ -1,8 +1,9 @@
 package com.soulittude.e_commerce.controller;
 
+import com.soulittude.e_commerce.dto.LoginRequest;
+import com.soulittude.e_commerce.dto.RegisterRequest;
 import com.soulittude.e_commerce.security.JwtUtils;
 import com.soulittude.e_commerce.service.AuthService;
-import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -40,19 +41,5 @@ public class AuthController {
         final UserDetails userDetails = authService.loadUserByUsername(request.getUsername());
         final String token = jwtUtils.generateToken(userDetails);
         return ResponseEntity.ok(Map.of("token", token));
-    }
-
-    // Add getters/setters with Lombok
-    @Data
-    static class RegisterRequest {
-        private String username;
-        private String email;
-        private String password;
-    }
-
-    @Data
-    static class LoginRequest {
-        private String username;
-        private String password;
     }
 }
