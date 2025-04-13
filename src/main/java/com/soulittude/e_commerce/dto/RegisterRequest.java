331a221040214cb1adbf5315e62743622d9ca1b4
soulittude <79145776+Soulittude@users.noreply.github.com>
2025-04-13 22:15:27 +0300

    Add RegisterRequest DTO with fields for username, email, and password

--- src/main/java/com/soulittude/e_commerce/dto/RegisterRequest.java
+++ src/main/java/com/soulittude/e_commerce/dto/RegisterRequest.java
@@ -1,5 +1,10 @@
 package com.soulittude.e_commerce.dto;
 
+import lombok.Data;
+
+@Data
 public class RegisterRequest {
-    
+    private String username;
+    private String email;
+    private String password;
 }
