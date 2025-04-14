f57e4e452bf76712f30d7b9748c750292cb56135
soulittude <79145776+Soulittude@users.noreply.github.com>
2025-04-14 22:37:04 +0300

    Add Cart and CartItem entities with relationships to User and Product

---
+++ src/main/java/com/soulittude/e_commerce/entity/CartItem.java
@@ -0,0 +1,20 @@
+package com.soulittude.e_commerce.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class CartItem {
+    @Id @GeneratedValue
+    private Long id;
+    
+    @ManyToOne
+    private Product product;
+    
+    private int quantity;
+    
+    @ManyToOne
+    private Cart cart;
+}
